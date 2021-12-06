96a6eff0f085e27e69143de59a028ec1cde55c81
Andy Grigg <andrew.grigg@ansys.com>
2021-12-06 18:04:38 -0500

    Let allowed_types check one object against multiple types

--- tests/test_allowed_types.py
+++ tests/test_allowed_types.py
@@ -4,50 +4,41 @@ from ansys.grantami.bomanalytics._allowed_types import allowed_types, _check_typ
 
 
 def test_allowed_types_args():
-    @allowed_types(int, int, int)
-    def test_function(*args, **kwargs):
-        return {"args": args, "kwargs": kwargs}
+    @allowed_types(int)
+    def test_function(*args):
+        return args
 
-    result = test_function(12, 23, 34)["args"]
-    assert result == (12, 23, 34)
+    # The function is expected to be a method, so "self" fills the expected role of self as the first positional arg
+    result = test_function("self", 23)
+    assert result == ("self", 23)
 
 
 def test_allowed_types_kwargs():
-    @allowed_types(int, int, int)
-    def test_function(*args, **kwargs):
-        return {"args": args, "kwargs": kwargs}
-
-    result = test_function(a=12, b=23, c=34)["kwargs"]
-    assert result == {"a": 12, "b": 23, "c": 34}
-
-
-def test_allowed_types_args_kwargs():
-    @allowed_types(int, int, int, int, int)
-    def test_function(*args, **kwargs):
-        return {"args": args, "kwargs": kwargs}
-
-    result = test_function(12, 23, a=34, b=45, c=56)
-    assert result["args"] == (12, 23)
-    assert result["kwargs"] == {"a": 34, "b": 45, "c": 56}
-
-
-def test_different_lengths_value_error():
     @allowed_types(int)
     def test_function(*args, **kwargs):
-        return {"args": args, "kwargs": kwargs}
+        return kwargs
 
-    with pytest.raises(ValueError) as e:
-        test_function(12, 23)
-    assert f"Number of types (1) does not match number of arguments (2)" == str(e.value)
+    result = test_function("self", value=23)
+    assert result == {"value": 23}
+
+
+def test_different_lengths_type_error():
+    @allowed_types(int)
+    def test_function(*args):
+        return args
+
+    with pytest.raises(TypeError) as e:
+        test_function("self", 23, 34)
+    assert 'The method "test_function" was called with 2 arguments.' in str(e.value)
 
 
 def test_different_types_type_error():
     @allowed_types(int)
-    def test_function(*args, **kwargs):
-        return {"args": args, "kwargs": kwargs}
+    def test_function(*args):
+        return args
 
     with pytest.raises(TypeError) as e:
-        test_function("test")
+        test_function("self", "test")
     assert f'Incorrect type for value "test". Expected "<class \'int\'>"' == str(e.value)
 
 
@@ -58,9 +49,11 @@ def test_different_types_type_error():
         (123, int),
         (123, Number),
         ([1, 2], [int]),
+        ([1, "2"], [int, str]),
         ((1, 2), (int,)),
         ((1, 2), (int, int)),
-        ({"a": 5.5, "b": 10.0}, {}),
+        ({1, 2}, {int}),
+        ({1, 2}, {int, int}),
         ({"a": 5.5, "b": 10.0}, {str: float}),
         ({"a": [1, 2], "b": [3, 4]}, {str: [int]}),
         ({"a": [1, 2], "b": [3, 4]}, object),
@@ -78,12 +71,29 @@ def test_check_type_success(obj, allowed_type):
         (123, str),
         ([1, 2], [float]),
         ([1, 2], [int, str]),
+        (["1", 2], [int, str]),
         ((5, 10), (str,)),
+        ({1, 2}, {str}),
         ({"a": 5.5, "b": 10.0}, []),
         ({"a": 5.5, "b": 10.0}, {str: int}),
         ({"a": [1, 2], "b": [3, 4]}, {str: [str]}),
     ],
 )
-def test_check_type_failure(obj, allowed_type):
+def test_check_type_wrong_type(obj, allowed_type):
     with pytest.raises(AssertionError):
+        _check_type(obj, allowed_type)
+
+
+@pytest.mark.parametrize(
+    "obj, allowed_type",
+    [
+        (("test",), (str, str)),
+        (("test", "test2"), (str, str, str)),
+        (("test", "test2", "test3"), (str, str)),
+        ({"a": 5.5, "b": 10.0}, {}),
+        ({1, 2.5}, {int, float}),
+    ],
+)
+def test_check_type_wrong_container_length(obj, allowed_type):
+    with pytest.raises(ValueError):
         _check_type(obj, allowed_type)

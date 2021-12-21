46f73259604a0dd69c6ae5bcf1c06e10f86ab000
Andy Grigg <andrew.grigg@ansys.com>
2021-12-21 16:34:18 -0500

    Fix exception type

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -3,13 +3,12 @@ import sys
 import subprocess
 import pathlib
 import backoff
-import requests
 
 pytestmark = pytest.mark.integration
 
 
 @backoff.on_exception(backoff.expo,
-                      requests.exceptions.ConnectionError,
+                      AssertionError,
                       max_tries=5)
 def test_examples(example_script: pathlib.Path):
     p = subprocess.Popen([sys.executable, str(example_script)], cwd=example_script.parent)  # str() needed in py <= 3.7

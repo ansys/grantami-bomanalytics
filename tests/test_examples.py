e60676793f2858611277219b8a80810ab5ad9d5d
Andy Grigg <andrew.grigg@ansys.com>
2021-12-21 16:26:52 -0500

    Add backoff on examples tests

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -2,10 +2,15 @@ import pytest
 import sys
 import subprocess
 import pathlib
+import backoff
+import requests
 
 pytestmark = pytest.mark.integration
 
 
+@backoff.on_exception(backoff.expo,
+                      requests.exceptions.ConnectionError,
+                      max_tries=5)
 def test_examples(example_script: pathlib.Path):
     p = subprocess.Popen([sys.executable, str(example_script)], cwd=example_script.parent)  # str() needed in py <= 3.7
     p.wait()

50be948c8347c98698fa16368b90744f40451441
Andy Grigg <andrew.grigg@ansys.com>
2021-12-22 09:14:57 -0500

    Remove 3.6 tests, pass env secrets into tox environments

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -2,14 +2,10 @@ import pytest
 import sys
 import subprocess
 import pathlib
-import backoff
 
 pytestmark = pytest.mark.integration
 
 
-@backoff.on_exception(backoff.constant,
-                      AssertionError,
-                      max_tries=5)
 def test_examples(example_script: pathlib.Path):
     p = subprocess.Popen([sys.executable, str(example_script)], cwd=example_script.parent)  # str() needed in py <= 3.7
     p.wait()

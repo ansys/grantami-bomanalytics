51e624f2b98b32e2f5a717bb453f736d4faa80e9
Andy Grigg <andrew.grigg@ansys.com>
2022-01-10 16:05:45 -0500

    Add a more fundamental example that includes messages. Also gives more background on fluent interfaces. Requires an alternative way of "faking" the connection with monkey patching.

---
+++ .ipython/profile_default/startup/10-monkeypatch-connection.py
@@ -0,0 +1,41 @@
+import os
+from ansys.grantami.bomanalytics import Connection
+from ansys.grantami.bomanalytics._connection import BomAnalyticsClient
+
+# Monkey patch the Connection() class to use the environment variable-specified server URL.
+original_ctor = Connection.__init__
+
+
+def new_init(self: Connection, _):
+    original_ctor(self, os.getenv("TEST_SL_URL"))
+
+
+Connection.__init__ = new_init
+
+# Monkey patch the Connection builder methods to use the environment variable-specified credentials.
+Connection.with_creds_original = Connection.with_credentials
+
+
+def with_credentials(self: Connection, _, __) -> Connection:
+    user_name = os.getenv("TEST_USER")
+    password = os.getenv("TEST_PASS")
+    return self.with_creds_original(user_name, password)
+
+
+def with_autologon(self: Connection) -> Connection:
+    return self.with_credentials("foo", "bar")
+
+
+Connection.with_credentials = with_credentials
+Connection.with_autologon = with_autologon
+
+
+# Monkey patch the Client object to report the url specified in the code, or the one below if not overridden
+server_url = "http://my_grantami_server/mi_servicelayer"
+
+
+def __repr__(self: BomAnalyticsClient) -> str:
+    return f'<BomServicesClient: url="{server_url}", dbkey="{self._db_key}">'
+
+
+BomAnalyticsClient.__repr__ = __repr__

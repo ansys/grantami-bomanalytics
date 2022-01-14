f9640d9e5945014d9b580ae80746cb685461a766
Andy Grigg <andrew.grigg@ansys.com>
2022-01-14 14:49:56 -0500

    Remove obsolete TODOs.

--- src/ansys/grantami/bomanalytics/__init__.py
+++ src/ansys/grantami/bomanalytics/__init__.py
@@ -2,4 +2,3 @@ from ._connection import Connection
 from ._exceptions import GrantaMIException
 
 __version__ = "0.1.0dev"
-# TODO use STK to extend BoM services objects? e.g. adding all references to sparsely referenced object

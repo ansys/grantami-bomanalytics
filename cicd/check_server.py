8339611edf5c4c84368ea8e8c6b5b82f8574b22b
Andy Grigg <andrew.grigg@ansys.com>
2022-02-04 09:13:28 -0500

    Start/stop integration test server on-demand (#65)
    * Check the server is healthy before testing
    
    * Install requests first
    
    * Try logging into azure
    
    * Start VM
    
    * Set account name
    
    * Change arg name, remove 'needs' to speed up debugging
    
    * Bump az cli version
    
    * Re-org  steps
    
    * Get the code to check the machine state
    
    * Stop VM once docs are built
    
    * Try a different way of logging in
    
    * Fix yaml
    
    * Don't start the VM if we're doing a dependency check only
    
    * Actually deallocate the VM
    
    * Change to trigger new action instance
    
    * Try and make VM stop conditional
    
    * Trivial commit to trigger cicd
    
    * Remove braces
    
    * Use a different method to determine whether to shut down the VM
    
    * Remove unneeded checkout
    
    * Trigger another action run
    
    * Revert change
    
    * Tidy up yaml
    
    * Log response status code from server / gateway
    
    * Make the ci/cd python logging a bit clearer
    
    * Fix attempt numbering
    
    * Clean up logs
    
    * Fix logging again

---
+++ cicd/check_server.py
@@ -0,0 +1,63 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import os
+import json
+import time
+from functools import wraps
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+MAX_ATTEMPTS = 30
+WAIT_TIME = 5
+
+
+def block_until_server_is_ok(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        check_count = 1
+        while True:
+            logger.info(f"Check {check_count}")
+            if func(*args, **kwargs):
+                logger.info(f"Server ready!")
+                break
+            if check_count == MAX_ATTEMPTS:
+                logger.info(f"Failed after 30 attempts. Quitting...")
+                raise ConnectionError
+            logger.info(f"Check {check_count} failed. Waiting 5 seconds...")
+            check_count = check_count + 1
+            time.sleep(WAIT_TIME)
+    return wrapper
+
+
+@block_until_server_is_ok
+def check_status(url: str, auth_header: HTTPBasicAuth) -> bool:
+    try:
+        response = requests.get(url + "/Health/v2.svc/", auth=auth_header)
+    except requests.exceptions.RequestException:
+        # This generally won't happen in normal operation. But if a RequestException happens we want to make sure we
+        # handle it and try again.
+        # If MI isn't running we'll generally get a 5xx status from the gateway instead, which is handled below.
+        return False
+    logger.info(f"Received {response.status_code} response.")
+    if response.status_code != 200:
+        return False
+
+    content = json.loads(response.content)
+    for check in content['HealthChecks']:
+        if check['Name'] == 'Database Check' and check['Status'] == 'Ok':
+            logger.info(f"All databases loaded.")
+            return True
+    logger.info(f"All databases not loaded.")
+    return False
+
+
+if __name__ == "__main__":
+    sl_url = os.getenv("TEST_SL_URL")
+    username = os.getenv("TEST_USER")
+    password = os.getenv("TEST_PASS")
+
+    logger.info(f"Checking if Granta MI server is ready for requests")
+    auth = HTTPBasicAuth(username, password)
+    check_status(sl_url, auth)

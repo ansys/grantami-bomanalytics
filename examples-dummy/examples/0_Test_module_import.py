ee147aa21f5ad91feff45a0a4766318c96538442
Andy Grigg <andrew.grigg@ansys.com>
2022-07-20 10:43:24 -0400

    Add dummy examples (#213)

---
+++ examples-dummy/examples/0_Test_module_import.py
@@ -0,0 +1,35 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# # Test Notebook 1
+
+# Check we can import the Connection class and instantiate some things
+
+# + tags=[]
+from ansys.grantami.bomanalytics import Connection
+
+server_url = "http://my_grantami_server/mi_servicelayer"
+cxn = Connection(server_url)
+cxn
+# -
+
+# Also try an indicator
+
+# + tags=[]
+from ansys.grantami.bomanalytics.indicators import RoHSIndicator
+
+indicator = RoHSIndicator(name="Indicator", legislation_names=["Legislation"])
+indicator
+# -
---
+++ examples-dummy/examples/index.rst
@@ -0,0 +1,16 @@
+.. _ref_grantami_bomanalytics_test_examples:
+
+Example Scripts
+===============
+
+Example build skipped
+
+The examples below are to validate the Jupyter notebook to HTML conversion.
+
+
+.. toctree::
+   :maxdepth: 2
+
+   0_Test_module_import
+   1_Test_Example/index
+

3c328ac1116839d84983ed64f09f5723a4b0ef65
Doug Addy <doug.addy@ansys.com>
2023-08-15 14:57:25 +0100

    Add scripts to automate backup and restoration of the test database

---
+++ cicd/get_cleaned_db_entries.py
@@ -0,0 +1,127 @@
+import json
+import logging
+from pathlib import Path
+
+from GRANTA_MIScriptingToolkit import granta as mpy
+from ansys.grantami.serverapi_openapi import api, models
+
+from cicd.connection import Connection
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+MI_URL = "http://localhost/mi_servicelayer"
+DB_KEY = "MI_Restricted_Substances"
+OUTPUT_FILE_NAME = Path("./rs_data.json").resolve()
+
+logger.info(f"Connecting to MI at {MI_URL} with AutoLogon")
+s = mpy.Session(MI_URL, autologon=True)
+db = s.get_db(db_key=DB_KEY)
+api_client = Connection(api_url=MI_URL).with_autologon().connect()
+
+tables_api = api.SchemaTablesApi(api_client)
+attributes_api = api.SchemaAttributesApi(api_client)
+layouts_api = api.SchemaLayoutsApi(api_client)
+layout_sections_api = api.SchemaLayoutSectionsApi(api_client)
+
+table_information = {
+    "MaterialUniverse": {"layout": "All attributes", "subset": "All materials"},
+    "Materials - in house": {"layout": "All attributes", "subset": "All materials"},
+    "Products and parts": {"layout": "All attributes", "subset": "All products and parts"},
+    "Specifications": {"layout": "All properties", "subset": "All specifications"},
+    "Coatings": {"layout": "All properties", "subset": "All coatings"},
+    "Restricted Substances": {"layout": "Restricted substances", "subset": "All substances"},
+    "Legislations and Lists": {"layout": "Legislations", "subset": "All legislations"},
+}
+
+extra_attributes = {"Coatings": ["Coating Code"], "Legislations and Lists": ["Legislation ID", "Short title"]}
+
+renamed_attributes = {"Specifications": {"Chemical name": "Substance name"}}
+
+info = {}
+logger.info(f"Reading records and attributes from database '{DB_KEY}'")
+logger.info("Getting Table Information")
+tables: models.GrantaServerApiSchemaTablesInfo = tables_api.v1alpha_databases_database_key_tables_get(
+    database_key=DB_KEY
+)
+table_name_map = {table.name: table for table in tables.tables}
+for table_name, table_details in table_information.items():
+    logger.info(f"--Processing table '{table_name}'")
+    table = db.get_table(table_name)
+    layout_name = table_details["layout"]
+    subset_name = table_details["subset"]
+    logger.info(f"----Fetching attributes in layout '{layout_name}")
+    renamed_attributes_for_table = renamed_attributes.get(table_name, [])
+    table_info = table_name_map[table_name]
+    table_layouts: models.GrantaServerApiSchemaLayoutsLayoutsInfo = (
+        layouts_api.v1alpha_databases_database_key_tables_table_guid_layouts_get(
+            database_key=DB_KEY, table_guid=table_info.guid
+        )
+    )
+    layout_map = {layout.name: layout for layout in table_layouts.layouts}
+    layout_item = layout_map[layout_name]
+    sections_response: models.GrantaServerApiSchemaLayoutsLayoutSectionsInfo = (
+        layout_sections_api.v1alpha_databases_database_key_tables_table_guid_layouts_layout_guid_sections_get(
+            database_key=DB_KEY, table_guid=table_info.guid, layout_guid=layout_item.guid, show_full_detail=True
+        )
+    )
+    layout_info = [section.to_dict() for section in sections_response.layout_sections]
+    if table_name in extra_attributes:
+        # Add extra attribute information to a section in the layout
+        table_attributes: models.GrantaServerApiSchemaAttributesAttributesInfo = (
+            attributes_api.v1alpha_databases_database_key_tables_table_guid_attributes_get(
+                DB_KEY, table_guid=table_info.guid
+            )
+        )
+        attribute_name_map = {attribute.name: attribute for attribute in table_attributes.attributes}
+        added_items = []
+        relevant_attribute_names = extra_attributes[table_name]
+        for extra_attribute_name in relevant_attribute_names:
+            attribute_info: models.GrantaServerApiSchemaSlimEntitiesSlimAttribute = attribute_name_map[
+                extra_attribute_name
+            ]
+            added_items.append(
+                models.GrantaServerApiSchemaLayoutsLayoutAttributeItem(
+                    attribute_type=attribute_info.type,
+                    underlying_entity_guid=attribute_info.guid,
+                    name=attribute_info.name,
+                    meta_attributes=[],
+                    tabular_columns=None,
+                )
+            )
+        layout_info.append(
+            models.GrantaServerApiSchemaLayoutsFullLayoutSection(
+                name="Extra Attributes", section_items=added_items
+            ).to_dict()
+        )
+    if len(renamed_attributes_for_table) > 0:
+        logger.info(f"----Remapping attribute names")
+        for section in layout_info:
+            for item in section["section_items"]:
+                if item["name"] in renamed_attributes_for_table:
+                    new_name = renamed_attributes_for_table[item["name"]]
+                    logger.info(f"------Renaming attribute '{item['name']}' to '{new_name}")
+                    item["name"] = renamed_attributes_for_table[item["name"]]
+                if "tabular_columns" in item and item["tabular_columns"] is not None:
+                    for column in item["tabular_columns"]:
+                        if column["name"] in renamed_attributes_for_table:
+                            new_name = renamed_attributes_for_table[column["name"]]
+                            logger.info(
+                                f"------Renaming tabular column '{column['name']}' to '{new_name}' in attribute '{item['name']}'"  # noqa: E501
+                            )
+                            column["name"] = renamed_attributes_for_table[column["name"]]
+    logger.info(f"----Fetching records in subset '{subset_name}'")
+    records = [
+        (record.history_guid, record.record_guid)
+        for record in table.all_records(include_folders=True, include_generics=True)
+    ]
+    info[table_name] = {"layout": layout_info, "records": records}
+
+logger.info(f"Finished processing data, writing to {OUTPUT_FILE_NAME}")
+with open(OUTPUT_FILE_NAME, "w", encoding="utf8") as fp:
+    json.dump(info, fp)
---
+++ cicd/modify_custom_rs_db.py
@@ -0,0 +1,173 @@
+import logging
+
+import GRANTA_MIScriptingToolkit as gdl
+
+from ansys.grantami.serverapi_openapi import api, models
+
+from cicd.connection import Connection
+from cicd.prepare_rs_db import TableBrowser
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+CUSTOM_DB_KEY = "MI_Restricted_Substances_Custom_Tables"
+
+if __name__ == "__main__":
+    logger.info("Renaming tables")
+    table_name_remapping = {
+        "MaterialUniverse": "My Material Universe",
+        "Materials - in house": "My Materials",
+        "Specifications": "specs",
+        "Products and parts": "Parts 'n' Products",
+        "Restricted Substances": "Chemicals",
+        "Coatings": "Coverings",
+    }
+
+    URL = "http://localhost/mi_servicelayer"
+    api_client = Connection(api_url=URL).with_autologon().connect()
+    gdl_session = gdl.GRANTA_MISession(url=URL, autoLogon=True)
+
+    database_client = api.SchemaDatabasesApi(api_client)
+
+    logger.info("Renaming Database")
+    database_info: models.GrantaServerApiSchemaDatabase = database_client.v1alpha_databases_database_key_get(
+        database_key=CUSTOM_DB_KEY
+    )
+    guid = database_info.guid
+    new_name = "Restricted Substances Custom Tables"
+    rename_request = models.GrantaServerApiSchemaUpdateDatabase(name=new_name)
+
+    database_client.v1alpha_databases_database_key_patch(database_key=CUSTOM_DB_KEY, body=rename_request)
+
+    table_browser = TableBrowser(api_client, logger)
+    custom_table_name_map = table_browser.get_table_name_guid_map(CUSTOM_DB_KEY)
+
+    for old_name, new_name in table_name_remapping.items():
+        table_guid = custom_table_name_map[old_name]
+        table_browser.update_table_name(db_key=CUSTOM_DB_KEY, table_guid=table_guid, new_table_name=new_name)
+
+    logger.info("Duplicating styrene record then withdrawing it. (TestActAsReadUser)")
+    data_import_service = gdl_session.dataImportService
+    substances_guid = custom_table_name_map["Chemicals"]
+    copy_import_record = gdl.ImportRecord(
+        existingRecord=gdl.RecordReference(
+            DBKey=CUSTOM_DB_KEY,
+            lookupValue=gdl.LookupValue(
+                attributeReference=gdl.AttributeReference(
+                    DBKey=CUSTOM_DB_KEY,
+                    name="CAS number",
+                    partialTableReference=gdl.PartialTableReference(tableGUID=substances_guid),
+                ),
+                attributeValue="100-42-5",
+            ),
+        ),
+        copyDestinationParent=gdl.RecordReference(
+            DBKey=CUSTOM_DB_KEY,
+            lookupValue=gdl.LookupValue(
+                attributeReference=gdl.AttributeReference(
+                    DBKey=CUSTOM_DB_KEY,
+                    name="CAS number",
+                    partialTableReference=gdl.PartialTableReference(tableGUID=substances_guid),
+                ),
+                attributeValue="7664-93-9",
+            ),
+        ),
+        recordName="Styrene Copy",
+        releaseRecord=True,
+        importRecordMode="Copy",
+    )
+    copy_request = gdl.SetRecordAttributesRequest(importRecords=[copy_import_record])
+    copy_response = data_import_service.SetRecordAttributes(copy_request)
+
+    record_reference = copy_response.recordsImported[0].recordReference
+    withdrawal_request = gdl.DeleteOrWithdrawIfLatestRecordVersionRequest(
+        deleteOrWithdrawRecords=[gdl.DeleteOrWithdrawRecord(recordReference=record_reference)]
+    )
+    delete_response = data_import_service.DeleteOrWithdrawIfLatestRecordVersion(withdrawal_request)
+
+    logger.info("Creating linked specifications. (TestSpecificationLinkDepth)")
+    specs_guid = custom_table_name_map["specs"]
+
+    tabular_type = gdl.TabularDataType()
+    tabular_type.AddColumn("Thickness")
+    new_row = tabular_type.CreateRow()
+
+    new_row.linkingValue = "Coating-203"
+    new_row.cells[0].data = gdl.RangeDataType(low=0.0508, high=0.127, unitSymbol="mm")
+    tabular_type.tabularDataRows.append(new_row)
+
+    spec_link_attribute = gdl.TabularDataType()
+    new_spec_row = spec_link_attribute.CreateRow()
+    new_spec_row.linkingValue = "MIL-DTL-53039,TypeI"
+    spec_link_attribute.tabularDataRows.append(new_spec_row)
+
+    import_spec_record = gdl.ImportRecord(
+        existingRecord=gdl.RecordReference(
+            DBKey=CUSTOM_DB_KEY,
+            lookupValue=gdl.LookupValue(
+                attributeReference=gdl.AttributeReference(
+                    DBKey=CUSTOM_DB_KEY,
+                    name="Specification ID",
+                    partialTableReference=gdl.PartialTableReference(tableGUID=specs_guid),
+                ),
+                attributeValue="MIL-DTL-53039",
+            ),
+        ),
+        recordName="MIL-DTL-53039, Type II",
+        releaseRecord=True,
+        importRecordMode="Create",
+        importAttributeValues=[
+            gdl.ImportAttributeValue(
+                attributeReference=gdl.AttributeReference(
+                    DBKey=CUSTOM_DB_KEY,
+                    name="Specification ID",
+                    partialTableReference=gdl.PartialTableReference(tableGUID=specs_guid),
+                ),
+                shortTextDataValue=gdl.ShortTextDataType(value="MIL-DTL-53039,TypeII"),
+            ),
+            gdl.ImportAttributeValue(
+                attributeReference=gdl.AttributeReference(
+                    pseudoAttribute=gdl.AttributeReference.MIPseudoAttributeReference.shortName
+                ),
+                shortTextDataValue=gdl.ShortTextDataType(value="MIL-DTL-53039, Type II"),
+            ),
+            gdl.ImportAttributeValue(
+                attributeReference=gdl.AttributeReference(
+                    DBKey=CUSTOM_DB_KEY,
+                    name="Coatings in this specification",
+                    partialTableReference=gdl.PartialTableReference(tableGUID=specs_guid),
+                ),
+                tabularDataValue=tabular_type,
+            ),
+            gdl.ImportAttributeValue(
+                attributeReference=gdl.AttributeReference(
+                    DBKey=CUSTOM_DB_KEY,
+                    name="Specifications in this specification",
+                    partialTableReference=gdl.PartialTableReference(tableGUID=specs_guid),
+                ),
+                tabularDataValue=spec_link_attribute,
+            ),
+            gdl.ImportAttributeValue(
+                attributeReference=gdl.AttributeReference(
+                    DBKey=CUSTOM_DB_KEY,
+                    name="Declaration type",
+                    partialTableReference=gdl.PartialTableReference(tableGUID=specs_guid),
+                ),
+                discreteDataValue=gdl.DiscreteDataType(discreteValues=[gdl.DiscreteValue(value="Generic data")]),
+            ),
+        ],
+        subsetReferences=[
+            gdl.SubsetReference(
+                DBKey=CUSTOM_DB_KEY,
+                name="All specifications",
+                partialTableReference=gdl.PartialTableReference(tableGUID=specs_guid),
+            )
+        ],
+    )
+    import_spec_request = gdl.SetRecordAttributesRequest(importRecords=[import_spec_record])
+    import_spec_response = data_import_service.SetRecordAttributes(import_spec_request)

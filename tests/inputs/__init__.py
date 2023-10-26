27af5cf03af284e8dca6a55e65818ec1e5fbab69
Ludovic Steinbach <ludovic.steinbach@ansys.com>
2023-10-26 12:37:42 +0200

    Remove duplicated BoM

--- tests/inputs/__init__.py
+++ tests/inputs/__init__.py
@@ -2,18 +2,17 @@ import pathlib
 
 from .examples import examples_as_dicts, examples_as_strings
 
+repository_root = pathlib.Path(__file__).parents[2]
+
 _bom_path = pathlib.Path(__file__).parent / "bom.xml"
 with open(_bom_path, "r") as f:
     sample_bom_1711 = f.read()
 
-_complex_bom_path = pathlib.Path(__file__).parent / "bom-complex.xml"
+_complex_bom_path = repository_root / "examples" / "3_Advanced_Topics" / "supporting-files" / "bom-complex.xml"
 with open(_complex_bom_path, "r") as f:
     sample_bom_complex = f.read()
 
 sample_bom_custom_db = sample_bom_complex.replace("MI_Restricted_Substances", "MI_Restricted_Substances_Custom_Tables")
-
-
-repository_root = pathlib.Path(__file__).parents[2]
 
 _bom_2301_path = repository_root / "examples" / "4_Sustainability" / "supporting-files" / "bom-2301-assembly.xml"
 with open(_bom_2301_path, "r") as f:

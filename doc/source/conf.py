2df524bbbc445f53be53ed3fbb77505d4e6d727f
Andy Grigg <andrew.grigg@ansys.com>
2024-11-15 08:20:44 -0500

    Update Ansys documentation links to 2025 R1 (#649)
    Co-authored-by: pyansys-ci-bot <92810346+pyansys-ci-bot@users.noreply.github.com>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -116,7 +116,7 @@ numpydoc_validation_exclude = {
 
 extlinks = {
     'MI_docs': (
-        'https://ansyshelp.ansys.com/account/secured?returnurl=/Views/Secured/Granta/v241/en/RS_and_Sustainability/%s',
+        'https://ansyshelp.ansys.com/account/secured?returnurl=/Views/Secured/Granta/v251/en/RS_and_Sustainability/%s',
         None
     ),
     "OpenAPI-Common": ("https://openapi.docs.pyansys.com/version/stable/%s", None),

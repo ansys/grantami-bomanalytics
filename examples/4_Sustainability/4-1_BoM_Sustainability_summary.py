e7587e6dcaa5da6c2b6c1e2eb3fd8f221e640b39
Andy Grigg <andrew.grigg@ansys.com>
2025-03-10 08:57:53 -0400

    Update examples to include transport results (#728)
    Co-authored-by: pyansys-ci-bot <92810346+pyansys-ci-bot@users.noreply.github.com>

--- examples/4_Sustainability/4-1_BoM_Sustainability_summary.py
+++ examples/4_Sustainability/4-1_BoM_Sustainability_summary.py
@@ -5,7 +5,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.15.1
+#       jupytext_version: 1.16.7
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
@@ -16,7 +16,19 @@
 #
 # The following supporting files are required for this example:
 #
-# * [bom-2301-assembly.xml](supporting-files/bom-2301-assembly.xml)
+# * [sustainability-bom-2412.xml](supporting-files/sustainability-bom-2412.xml)
+
+# <div class="alert alert-info">
+#
+# **Info:**
+#
+# This example requires uses a 24/12 XML BoM, which requires Granta MI Restricted Substances and Sustainability Reports
+# 2025 R2 or later. Running this example as-is results in an error due to an unsupported BoM version.
+#
+# To run this example with an older version of the reports bundle, use
+# [sustainability-bom-2301.xml](supporting-files/sustainability-bom-2301.xml) instead. Some sections of this example
+# will produce different results from the published example when this BoM is used.
+# </div>
 
 # ## Run a BoM sustainability summary query
 #
@@ -33,7 +45,7 @@ cxn = Connection(server_url).with_credentials("user_name", "password").connect()
 # ``MJ`` for energy, ``kg`` for mass, and ``km`` for distance.
 
 # +
-xml_file_path = "supporting-files/bom-2301-assembly.xml"
+xml_file_path = "supporting-files/sustainability-bom-2412.xml"
 with open(xml_file_path) as f:
     bom = f.read()
 
@@ -95,7 +107,7 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 
-def plot_impact(df, title, textinfo="percent+label", hoverinfo="value+name"):
+def plot_impact(df, title, textinfo="percent+label", hoverinfo="value+name", labels=True):
     fig = make_subplots(
         rows=1,
         cols=2,
@@ -105,7 +117,8 @@ def plot_impact(df, title, textinfo="percent+label", hoverinfo="value+name"):
     fig.add_trace(go.Pie(labels=df["Name"], values=df[EE_HEADER], name=ENERGY_UNIT), 1, 1)
     fig.add_trace(go.Pie(labels=df["Name"], values=df[CC_HEADER], name=MASS_UNIT), 1, 2)
     fig.update_layout(title_text=title, legend=dict(orientation="h"))
-    fig.update_traces(textposition="inside", textinfo=textinfo, hoverinfo=hoverinfo)
+    if labels:
+        fig.update_traces(textposition="inside", textinfo=textinfo, hoverinfo=hoverinfo)
     fig.show()
 
 
@@ -115,14 +128,17 @@ plot_impact(phases_df, "BoM sustainability summary - By phase")
 # ## Transport phase
 #
 # The environmental contribution from the transport phase is summarized in the ``transport_details`` property. Results
-# include the individual environmental impact for each transport stage included in the input BoM.
+# include the individual environmental impact for each transport stage included in the input BoM. A BoM may include
+# many transport stages, each describing transportation throughout the product lifecycle. Print the first three only.
 
-sustainability_summary.transport_details
+sustainability_summary.transport_details[:3]
+
+# Convert all to a DataFrame and describe the result
 
 # +
 DISTANCE_HEADER = f"Distance [{DISTANCE_UNIT}]"
 
-transport_df = pd.DataFrame.from_records(
+transport_df_full = pd.DataFrame.from_records(
     [
         {
             "Name": item.name,
@@ -135,10 +151,32 @@ transport_df = pd.DataFrame.from_records(
         for item in sustainability_summary.transport_details
     ]
 )
+transport_df_full.describe()
+# -
+
+# Most of these transport stages contribute little to the overall sustainability impact. To make a visualization more
+# insightful, group all transport stages that contribute less than 5% of embodied energy or climate change in a single
+# 'Other' transport stage.
+
+# +
+# Define the criterion
+criterion = (transport_df_full["EE%"] < 5.0) | (transport_df_full["CC%"] < 5.0)
+
+# Aggregate all rows that meet the criterion
+transport_df_below_5_pct = transport_df_full.loc[criterion].sum(numeric_only=True).to_frame().T
+transport_df_below_5_pct["Name"] = "Other"
+
+# Sort all rows that do not meet the criterion by embodied energy
+transport_df_over_5_pct = transport_df_full.loc[~(criterion)].sort_values(by="EE%", ascending=False)
+
+# Concatenate the rows together
+transport_df = pd.concat([transport_df_over_5_pct, transport_df_below_5_pct], ignore_index=True)
 transport_df
 # -
 
-plot_impact(transport_df, "Transport stages - environmental impact")
+plot_impact(transport_df, "Transport stages - environmental impact", labels=False)
+
+# ### Transport impact per unit distance
 
 # In some situations, it might be useful to calculate the environmental impact per distance travelled and add the
 # results as new columns in the dataframe.
@@ -164,8 +202,84 @@ fig.update_layout(
     title_text="Transport stages impact - Relative to distance travelled",
     legend=dict(orientation="h")
 )
-fig.update_traces(textposition="inside", textinfo="percent+label", hoverinfo="value+name")
 fig.show()
+
+# ### Transport impact aggregated by category
+
+# <div class="alert alert-info">
+#
+# **Info:**
+#
+# This section requires Granta MI Restricted Substances and Sustainability Reports 2025 R2. Results will differ from
+# published examples if older software versions are used.
+# </div>
+
+# The environmental impacts from transportation associated with distribution and manufacturing phases are summarized in
+# the ``distribution_transport_summary`` and ``manufacturing_transport_summary`` properties.
+
+sustainability_summary.distribution_transport_summary
+
+# +
+dist_summary = sustainability_summary.distribution_transport_summary
+distribution = {
+    "Name": "Distribution",
+    DISTANCE_HEADER: dist_summary.distance.value,
+    "EE%": dist_summary.embodied_energy_percentage,
+    EE_HEADER: dist_summary.embodied_energy.value,
+    "CC%": dist_summary.climate_change_percentage,
+    CC_HEADER: dist_summary.climate_change.value,
+}
+
+manuf_summary = sustainability_summary.manufacturing_transport_summary
+manufacturing = {
+    "Name": "Manufacturing",
+    DISTANCE_HEADER: manuf_summary.distance.value,
+    "EE%": manuf_summary.embodied_energy_percentage,
+    EE_HEADER: manuf_summary.embodied_energy.value,
+    "CC%": manuf_summary.climate_change_percentage,
+    CC_HEADER: manuf_summary.climate_change.value,
+}
+
+transport_by_category_df = pd.DataFrame.from_records([distribution, manufacturing])
+transport_by_category_df
+# -
+
+plot_impact(transport_by_category_df, "Transport impact - grouped by category")
+
+# ### Transport impact aggregated by part
+
+# <div class="alert alert-info">
+#
+# **Info:**
+#
+# This section requires Granta MI Restricted Substances and Sustainability Reports 2025 R2. Results will differ from
+# published examples if older software versions are used.
+# </div>
+
+# The environmental contributions from transportation are summarized by the associated part in the
+# ``transport_details_aggregated_by_part`` property. This property groups parts that contribute less than 5% embodied
+# energy or climate change automatically.
+
+sustainability_summary.transport_details_aggregated_by_part
+
+transport_by_part_df = pd.DataFrame.from_records(
+    [
+        {
+            "Name": item.part_name,
+            "Parent part name": item.parent_part_name,
+            DISTANCE_HEADER: item.distance.value,
+            "EE%": item.embodied_energy_percentage,
+            EE_HEADER: item.embodied_energy.value,
+            "CC%": item.climate_change_percentage,
+            CC_HEADER: item.climate_change.value,
+            "Transport types": "; ".join(item.transport_types),
+        }
+        for item in sustainability_summary.transport_details_aggregated_by_part
+    ]
+)
+transport_by_part_df
+
+plot_impact(transport_by_part_df, "Transport impact - grouped by part")
 
 # ## Materials phase
 #
--- tests/conftest.py
+++ tests/conftest.py
@@ -20,32 +20,27 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+from collections import defaultdict
 import os
 import pathlib
-from typing import List, cast
+from typing import List
 
-from defusedxml import ElementTree
 import pytest
 import requests_mock
 
 from ansys.grantami.bomanalytics import Connection
 
-from .common import CUSTOM_TABLES, LICENSE_RESPONSE
-
-sl_url = os.getenv("TEST_SL_URL", "http://localhost/mi_servicelayer")
-read_username = os.getenv("TEST_USER")
-read_password = os.getenv("TEST_PASS")
-
-write_username = os.getenv("TEST_WRITE_USER")
-write_password = os.getenv("TEST_WRITE_PASS")
-
-
-def _get_connection(url, username, password):
-    if username is not None:
-        connection = Connection(api_url=url).with_credentials(username, password).connect()
-    else:
-        connection = Connection(api_url=url).with_autologon().connect()
-    return connection
+from .common import (
+    CUSTOM_TABLES,
+    LICENSE_RESPONSE,
+    _get_connection,
+    get_mi_version,
+    read_password,
+    read_username,
+    sl_url,
+    write_password,
+    write_username,
+)
 
 
 @pytest.fixture
@@ -96,12 +91,34 @@ def pytest_generate_tests(metafunc):
     'example_script' fixture.
     """
 
+    mi_version = get_mi_version()
+
+    # If we don't have an MI version, we cannot contact an MI server. Example tests cannot run, so we might as well
+    # return early.
+    if mi_version is None:
+        return
+
+    skip_examples_for_version = defaultdict(set)
+    skip_examples_for_version[(25, 1)] = {"4-1_BoM_Sustainability_summary.py", "4-2_BoM_Sustainability.py"}
+    skip_examples_for_version[(24, 2)] = {"4-1_BoM_Sustainability_summary.py", "4-2_BoM_Sustainability.py"}
+
     if "example_script" in metafunc.fixturenames:
         this_file = pathlib.Path(__file__).parent.resolve()
         example_path = this_file / pathlib.Path("../examples")
-        output_files = discover_python_scripts(example_path)
-        file_names = [str(file) for file in output_files]
-        metafunc.parametrize("example_script", output_files, ids=file_names)
+        parameters = []
+        example_files = discover_python_scripts(example_path)
+        for example_file in example_files:
+            xfail_examples = skip_examples_for_version[mi_version]
+            if example_file.name in xfail_examples:
+                formatted_version = ".".join(str(x) for x in mi_version)
+                mark = pytest.mark.skip(
+                    f'Example "{example_file.name}" skipped for Granta MI release version "{formatted_version}"'
+                )
+                param = pytest.param(example_file, id=example_file.name, marks=mark)
+            else:
+                param = pytest.param(example_file, id=example_file.name)
+            parameters.append(param)
+        metafunc.parametrize("example_script", parameters)
 
 
 def discover_python_scripts(example_dir: pathlib.Path) -> List[pathlib.Path]:
@@ -123,28 +140,7 @@ def discover_python_scripts(example_dir: pathlib.Path) -> List[pathlib.Path]:
 
 @pytest.fixture(scope="session")
 def mi_version() -> tuple[int, int] | None:
-    """The version of MI referenced by the test url.
-
-    Returns
-    -------
-    tuple[int, int] | None
-        A 2-tuple containing the (MAJOR, MINOR) Granta MI release version, or None if a test URL is not available.
-
-    Notes
-    -----
-    This fixture returns None if the ``sl_url`` variable is not available. This is typically because the tests are
-    running in CI and the TEST_SL_URL environment variable was not populated.
-    """
-    if os.getenv("CI") and not os.getenv("TEST_SL_URL"):
-        return None
-    connection = _get_connection(sl_url, read_username, read_password)
-    session = connection.rest_client
-    response = session.get(connection._sl_url + "/SystemInfo/v4.svc/Versions/Mi")
-    tree = ElementTree.fromstring(response.text)
-    version = next(c.text for c in tree if c.tag.rpartition("}")[2] == "MajorMinorVersion")
-    parsed_version = [int(v) for v in version.split(".")]
-    assert len(parsed_version) == 2
-    return cast(tuple[int, int], tuple(parsed_version))
+    return get_mi_version()
 
 
 @pytest.fixture(autouse=True)

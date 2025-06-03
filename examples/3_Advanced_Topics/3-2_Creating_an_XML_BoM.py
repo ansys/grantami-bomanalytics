1c362d58589c58292b2af93ccff8bf210a42ae52
Andy Grigg <andrew.grigg@ansys.com>
2025-06-03 08:17:15 -0400

    Revert "Add an example of creating a BoM from Python classes directly - Review (#809)"
    This reverts commit 91fe7ecddf7a7e62ee16fd22c28326326b8d2c0e.

--- examples/3_Advanced_Topics/3-2_Creating_an_XML_BoM.py
+++ examples/3_Advanced_Topics/3-2_Creating_an_XML_BoM.py
@@ -22,12 +22,10 @@
 # The door contains two hinges and a handle, both fixed to the frame with machine screws and
 # washers, the door glass is coated with a partially reflective polymer film.
 
-# Most Granta MI deployments will use the default database key and table names:
+# Most installations of Granta MI will use the default database key and table names:
 
 DB_KEY = "MI_Restricted_Substances"
 TABLE_NAME = "MaterialUniverse"
-
-# ## Create the Bill of Materials as Python objects
 
 # The structure of an XML BoM is hierarchical, individual parts belong to assemblies which can
 # belong to larger assemblies. It is possible to construct the BoM in one statement, but this
@@ -216,9 +214,7 @@ door_assembly = Part(
     ]
 )
 
-# ## Serialize the BoM to XML
-
-# Generate a BoM from the door assembly part, and serialize the BoM to XML.
+# Generate a BoM from the door assembly part, and dump the BoM to XML.
 
 # +
 from ansys.grantami.bomanalytics import BoMHandler
@@ -229,8 +225,6 @@ bom_handler = BoMHandler()
 rendered_bom = bom_handler.dump_bom(door_assembly_bom)
 rendered_bom.splitlines()[0:10]
 # -
-
-# ## Run a compliance query
 
 # Now that you have created an XML BoM, run a compliance query to determine whether the BoM complies
 # with a specific legislation.

65fc6dbbc948251065327fbf5b36d14e0b73f33c
Andy Grigg <agrigg@synopsys.com>
2026-09-17 17:00:40 -0400

    Fix linting failures

--- doc/class_documenter.py
+++ doc/class_documenter.py
@@ -1,13 +1,8 @@
 from inspect import getmodule
 from typing import Any, Optional
 
-from sphinx.ext.autodoc import (
-    Documenter,
-    MethodDocumenter,
-    PropertyDocumenter,
-    member_order_option,
-)
 from sphinx.ext.autodoc import ClassDocumenter as DefaultClassDocumenter
+from sphinx.ext.autodoc import Documenter, MethodDocumenter, PropertyDocumenter, member_order_option
 
 try:
     from sphinx.ext.autodoc import ModuleAnalyzer
